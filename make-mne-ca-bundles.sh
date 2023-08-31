14f5b6572a51ed7c11e5d642f887f1ad2f6faf22
Djordje Zekovic <dj@zeko.me>
2023-08-31 17:47:34 +0200

    new TL_ME issued seq 12

--- make-mne-ca-bundles.sh
+++ make-mne-ca-bundles.sh
@@ -12,7 +12,7 @@
 set -euo pipefail
 
 # adjust on next known update (!!!)
-TRUST_LIST_URL=https://wapi.gov.me/download/9731e700-cad1-4c63-b5a0-5fd82122cf30?version=1.0
+TRUST_LIST_URL=https://wapi.gov.me/download/06af19ec-36b8-4a64-a215-d6eef2d913d3?version=1.0
 TRUSTED_SIGNER_URL=https://wapi.gov.me/download/133d6553-1d2b-4302-833d-3f359441ee2b?version=1.0
 
 # filenames
